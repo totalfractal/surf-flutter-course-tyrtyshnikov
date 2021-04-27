d25bd5397bb8520eb552f1967f6da790d8ea0493
Andrey Tyrtyshnikov <an.tyrtyshnikov@gmail.com>
2021-04-27 14:07:05 +0300

    Image Network And CliRRect

--- lib/ui/screen/sight_card.dart
+++ lib/ui/screen/sight_card.dart
@@ -15,86 +15,120 @@ class SightCard extends StatelessWidget {
     return Container(
       padding: EdgeInsets.all(16.0),
       //height: MediaQuery.of(context).size.width * 0.5,
-      child: AspectRatio(
-        aspectRatio: 3 / 2,
-        child: Container(
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.all(16.0),
-                height: 96,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(16),
-                      topRight: Radius.circular(16)),
-                  color: Colors.red,
-                ),
-                child: Container(
-                  alignment: Alignment.topCenter,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        child: Text(
-                            sight.type.isEmpty
-                                ? "категория"
-                                : sight.type.toLowerCase(),
-                            style: TextStyle(
-                              fontFamily: 'Roboto',
-                              fontStyle: FontStyle.normal,
-                              fontSize: 14,
-                              fontWeight: FontWeight.w700,
-                              color: Colors.white,
-                            )),
-                      ),
-                      Container(
-                        color: Colors.green,
-                        width: 20,
-                        height: 20,
-                      ),
+
+      child: Container(
+        child: Column(
+          children: [
+            Container(
+              height: 96,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    topRight: Radius.circular(16)),
+                gradient: new LinearGradient(
+                    colors: [
+                      Colors.black38,
+                      Colors.black12,
                     ],
+                    begin: const FractionalOffset(0.0, 0.0),
+                    end: const FractionalOffset(1.0, 0.0),
+                    stops: [0.0, 1.0],
+                    tileMode: TileMode.clamp),
+              ),
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(16),
+                        topRight: Radius.circular(16)),
+                    child: Image.network(
+                      sight.url,
+                      loadingBuilder: (BuildContext context, Widget child,
+                          ImageChunkEvent loadingProgress) {
+                        if (loadingProgress == null) {
+                          return child;
+                        }
+                        return Center(
+                          child: CircularProgressIndicator(
+                            value: loadingProgress.expectedTotalBytes != null
+                                ? loadingProgress.cumulativeBytesLoaded /
+                                    loadingProgress.expectedTotalBytes
+                                : null,
+                          ),
+                        );
+                      },
+                      fit: BoxFit.fitWidth,
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                padding: EdgeInsets.all(16.0),
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                  Container(
+                    padding: EdgeInsets.all(16.0),
+                    alignment: Alignment.topCenter,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Container(
-                          padding: EdgeInsets.symmetric(vertical: 2),
-                          child:
-                              Text(sight.name.isEmpty ? "Название" : sight.name,
-                                  style: TextStyle(
-                                    fontFamily: 'Roboto',
-                                    fontStyle: FontStyle.normal,
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w500,
-                                    color: hexToColor("#3B3E5B"),
-                                  )),
+                          child: Text(
+                              sight.type.isEmpty
+                                  ? "категория"
+                                  : sight.type.toLowerCase(),
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontStyle: FontStyle.normal,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white,
+                              )),
                         ),
                         Container(
-                          child: Text("закрыто до 9:00",
-                              textAlign: TextAlign.start,
-                              style: TextStyle(
+                          color: Colors.green,
+                          width: 20,
+                          height: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.all(16.0),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 2),
+                        child:
+                            Text(sight.name.isEmpty ? "Название" : sight.name,
+                                style: TextStyle(
                                   fontFamily: 'Roboto',
                                   fontStyle: FontStyle.normal,
                                   fontSize: 14,
-                                  fontWeight: FontWeight.normal,
-                                  color: hexToColor("#7C7E92"),
-                                  decoration: TextDecoration.none)),
-                        ),
-                      ]),
-                ),
+                                  fontWeight: FontWeight.w500,
+                                  color: hexToColor("#3B3E5B"),
+                                )),
+                      ),
+                      Container(
+                        child: Text("закрыто до 9:00",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontStyle: FontStyle.normal,
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal,
+                                color: hexToColor("#7C7E92"),
+                                decoration: TextDecoration.none)),
+                      ),
+                    ]),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
